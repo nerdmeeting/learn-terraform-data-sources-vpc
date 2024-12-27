4e5cf6286f8567d100e7c4307cc8511ec47daf66
Nathan Bills <nathan@nerdmeeting.com>
2024-12-26 18:13:07 -0700

    Change the hard-coded availability zones to a data lookup so the region can be successfully changed if desired.

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ module "vpc" {
 
   cidr = var.vpc_cidr_block
 
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1e"]
+  azs             = data.aws_availability_zones.available.names
   private_subnets = slice(var.private_subnet_cidr_blocks, 0, 2)
   public_subnets  = slice(var.public_subnet_cidr_blocks, 0, 2)
 
@@ -41,4 +41,13 @@ module "lb_security_group" {
   vpc_id      = module.vpc.vpc_id
 
   ingress_cidr_blocks = ["0.0.0.0/0"]
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+  
+  filter = {
+    name = "zone-type"
+    values = ["availability-zone"]
+  }
 }
